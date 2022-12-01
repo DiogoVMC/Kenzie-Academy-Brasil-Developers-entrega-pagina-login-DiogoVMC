4178d6127d7e6440ff704e4ece554f4c24c75a6c
Diogo <diogocano.martins@gmail.com>
2022-12-01 18:09:21 -0300

    primeiro commit

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,11 @@
-/* Desenvolva seu código aqui... */
+
+const buttonToggleModal = document.querySelectorAll("[data-modal-control]")
+
+for(i=0; i<buttonToggleModal.length; i++){
+    let button=buttonToggleModal[i]
+    button.addEventListener("click", function(){
+       let valueDataModalControl = button.getAttribute("data-modal-control")
+       document.getElementById(valueDataModalControl).classList.toggle("show-modal")
+    
+    })
+}
